501fee91f347b23ad2ac5e0b7c80d572787998cf
Kaike <kaiketinocon@outlook.com>
2025-01-09 14:11:39 -0300

    alteração no nome "Checkout" para "Checkin" adicionado anotações de nom de tabela e entidade, também EqualsAndHashCode nas duas entidades

--- hotel/src/main/java/com/desafio/hotel/entity/checkin/Checkin.java
+++ hotel/src/main/java/com/desafio/hotel/entity/checkin/Checkin.java
@@ -1,19 +1,25 @@
-package com.desafio.hotel.entity;
+package com.desafio.hotel.entity.checkin;
 
+import com.desafio.hotel.entity.guest.Guest;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity
+
+@Entity(name = "Checkin")
+@Table(name = "Checkins")
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
-public class Checkout {
+@EqualsAndHashCode(of = "id")
+public class Checkin {
 
     @Id
     @GeneratedValue
@@ -25,11 +31,11 @@ public class Checkout {
     @NotNull(message = "dataEntrada não pode estar vazio!")
     private LocalDateTime dataEntrada;
 
-    private LocalDateTime dataSaida;
-
     private boolean adicionalVeiculo;
 
+    private BigDecimal valorTotal;
 
+    private LocalDateTime dataSaida;
 
 
 
--- hotel/src/main/java/com/desafio/hotel/entity/guest/Guest.java
+++ hotel/src/main/java/com/desafio/hotel/entity/guest/Guest.java
@@ -1,19 +1,19 @@
-package com.desafio.hotel.entity;
+package com.desafio.hotel.entity.guest;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "Guest")
+@Table(name = "Guests")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Guest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
