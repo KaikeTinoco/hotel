9e15dcee1d578c09d1c88a3b2dcc411121605a7e
Kaike <kaiketinocon@outlook.com>
2025-01-09 23:25:01 -0300

    novo Dto para criação de Guests, e modificação nas importações da entidade

---
+++ hotel/src/main/java/com/desafio/hotel/dto/guest/GuestDto.java
@@ -0,0 +1,11 @@
+package com.desafio.hotel.dto.guest;
+
+import lombok.Data;
+
+@Data
+public class GuestDto {
+    private String nome;
+    private String documento;
+    private String telefone;
+
+}
