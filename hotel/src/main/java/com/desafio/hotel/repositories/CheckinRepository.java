459dd6b080ba98ef0301a1643fe52a30ab3c722a
Kaike <kaiketinocon@outlook.com>
2025-01-09 21:42:48 -0300

    novo método 'Optional<List<Checkin>> findByGuestId(Long guestId);'

--- hotel/src/main/java/com/desafio/hotel/repositories/CheckinRepository.java
+++ hotel/src/main/java/com/desafio/hotel/repositories/CheckinRepository.java
@@ -4,11 +4,13 @@ import com.desafio.hotel.entity.checkin.Checkin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CheckinRepository extends JpaRepository<Checkin, Long> {
     Optional<Checkin> findById(Long id);
+    Optional<List<Checkin>> findByGuestId(Long guestId);
     Optional<Checkin> findByAdicionalVeiculo(boolean adicionalVeiculo);
 
 }
--- hotel/src/main/java/com/desafio/hotel/repositories/CheckoutRepository.java
+++ hotel/src/main/java/com/desafio/hotel/repositories/CheckoutRepository.java
@@ -4,9 +4,11 @@ import com.desafio.hotel.entity.checkin.Checkin;
 import com.desafio.hotel.entity.checkout.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Optional<Checkout> findById(Long id);
+    Optional<List<Checkin>> findByGuestId(Long guestId);
     Optional<Checkout> findByAdicionalVeiculo(boolean adicionalVeiculo);
 }
