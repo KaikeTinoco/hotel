5ad495720f402118e5e2f76160bd5ee42a83a6b7
Kaike <kaiketinocon@outlook.com>
2025-01-09 23:25:43 -0300

    criação do metodo  Optional<List<Guest>> findByDentroHotel(boolean dentroHotel);

--- hotel/src/main/java/com/desafio/hotel/repositories/GuestRepository.java
+++ hotel/src/main/java/com/desafio/hotel/repositories/GuestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,5 @@ public interface GuestRepository extends JpaRepository<Guest, Long>, JpaSpecific
      Optional<Guest> findByNome(String nome);
      Optional<Guest> findByDocumento(String documento);
      Optional<Guest> findByTelefone(String telefone);
+     Optional<List<Guest>> findByDentroHotel(boolean dentroHotel);
 }
