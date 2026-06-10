9da7c081ebf3d32be69403f80db5e22a98519a5c
Kaike Tinoco Nantes <kaiketinocon@outlook.com.br>
2026-06-10 12:48:28 -0400

    test: add unit tests for LocalTokenService and UserRepository

---
+++ hotel/src/test/java/com/desafio/hotel/services/tokens/LocalTokenServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.desafio.hotel.services.tokens;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalTokenServiceImplTest {
+
+    @Test
+    void generateToken() {
+    }
+
+    @Test
+    void validateToken() {
+    }
+}
